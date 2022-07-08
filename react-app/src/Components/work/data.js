ef32fa36da2a9b1c2e074d8acda4eb0023b824a7
joaoguedes <joaoguedes.cjp@gmail.com>
2022-07-08 10:51:35 +0100

    Optimized Work Component code with data files and added project images
    Optimized Work Component code with data files and added project images

---
+++ react-app/src/Components/work/data.js
@@ -0,0 +1,66 @@
+export const data = [
+    {
+        title:{
+            pt:"Website Pessoal",
+            eng:"Personal Website"
+        },
+        subject:{
+            pt:"Desenvolvimento Web",
+            eng:"Web Development"
+        },
+        description:{
+            pt:"Este aplicativo foi construído usando a linguagem de programação R para prever com precisão o número de visitas que um shopping teria em uma determinada semana para que as campanhas de marketing apropriadas fossem configuradas durante cada dia.",
+            eng:"This app was built using the R programming language to accurately predict the number of visits a Mall would have in a certain week so the appropriate marketing campaigns would be set up during each day."
+        },
+        icon: "webdev",
+        image:"./img/personal-website.png"
+    },
+    {
+        title:{
+            pt:"Previsões em R",
+            eng:"R Forecasting"
+        },
+        subject:{
+            pt:"Machine Learning",
+            eng:"Machine Learning"
+        },
+        description:{
+            pt:"Este aplicativo foi construído usando a linguagem de programação R para prever com precisão o número de visitas que um shopping teria em uma determinada semana para que as campanhas de marketing apropriadas fossem configuradas durante cada dia.",
+            eng:"This app was built using the R programming language to accurately predict the number of visits a Mall would have in a certain week so the appropriate marketing campaigns would be set up during each day."
+        },
+        icon: "ml",
+        image:"./img/rForecasting.png"
+    },
+    {
+        title:{
+            pt:"Preditor ESRB",
+            eng:"ESRB Predictor"
+        },
+        subject:{
+            pt:"Machine Learning",
+            eng:"Machine Learning"
+        },
+        description:{
+            pt:"Este aplicativo foi construído usando a linguagem de programação R para prever com precisão o número de visitas que um shopping teria em uma determinada semana para que as campanhas de marketing apropriadas fossem configuradas durante cada dia.",
+            eng:"This app was built using the R programming language to accurately predict the number of visits a Mall would have in a certain week so the appropriate marketing campaigns would be set up during each day."
+        },
+        icon: "ml",
+        image:"./img/esrbpredictor.png"
+    },
+    {
+        title:{
+            pt:"Balanceamento de Carga de Servidores",
+            eng:"Web Server Load Balancing"
+        },
+        subject:{
+            pt:"Infraestrutura e Redes",
+            eng:"Networking and Infrastructure"
+        },
+        description:{
+            pt:"Este aplicativo foi construído usando a linguagem de programação R para prever com precisão o número de visitas que um shopping teria em uma determinada semana para que as campanhas de marketing apropriadas fossem configuradas durante cada dia.",
+            eng:"This app was built using the R programming language to accurately predict the number of visits a Mall would have in a certain week so the appropriate marketing campaigns would be set up during each day."
+        },
+        icon: "netinf",
+        image:"./img/loadbalancer.png"
+    }
+]
