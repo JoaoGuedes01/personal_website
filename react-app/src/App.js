e5ae7b64c4b02d2042e65289fe77054b7476c69b
joaoguedes <joaoguedes.cjp@gmail.com>
2022-07-19 06:00:00 +0100

    Added dark mode functionality to all components
    Added dark mode functionality to all components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -19,19 +19,42 @@ import {
   Contact,
   Footer,
   Loader,
+  WorkDetails,
 } from "./Components";
 
 const AnimatedRoutes = (props) => {
   const location = useLocation();
   const lang = props.lang;
+  const theme = props.theme;
   return (
     <AnimatePresence exitBeforeEnter>
       <Routes location={location} key={location.key}>
-        <Route exact path="/" element={<Aboutme lang={lang} />} />
-        <Route exact path="/work" element={<Work lang={lang} />} />
-        <Route exact path="/experience" element={<Experience />} />
-        <Route exact path="/education" element={<Education />} />
-        <Route exact path="/contacts" element={<Contact />} />
+        <Route exact path="/" element={<Aboutme lang={lang} theme={theme} />} />
+        <Route
+          exact
+          path="/work"
+          element={<Work lang={lang} theme={theme} />}
+        />
+        <Route
+          exact
+          path="/work/:id"
+          element={<WorkDetails lang={lang} theme={theme} />}
+        />
+        <Route
+          exact
+          path="/experience"
+          element={<Experience lang={lang} theme={theme} />}
+        />
+        <Route
+          exact
+          path="/education"
+          element={<Education lang={lang} theme={theme} />}
+        />
+        <Route
+          exact
+          path="/contacts"
+          element={<Contact lang={lang} theme={theme} />}
+        />
       </Routes>
     </AnimatePresence>
   );
@@ -39,18 +62,27 @@ const AnimatedRoutes = (props) => {
 
 const App = () => {
   const [lang, setLang] = useState("eng");
-  const [loading, setLoading] = useState(true);
+  const [theme, setTheme] = useState("light");
+  const [loading, setLoading] = useState(false);
 
   return (
-    <div>
+    <div
+      className={theme == "light" ? "light-body" : "dark-body"}
+      tabIndex="-1"
+    >
       <AnimatePresence exitBeforeEnter>
         {loading ? (
           <Loader key="loader" setLoading={setLoading} />
         ) : (
           <Router key="router">
-            <Navbar />
-            <AnimatedRoutes lang={lang} />
-            <Footer setLang={setLang} lang={lang} />
+            <Navbar lang={lang} theme={theme} />
+            <AnimatedRoutes lang={lang} theme={theme} />
+            <Footer
+              setLang={setLang}
+              lang={lang}
+              setTheme={setTheme}
+              theme={theme}
+            />
           </Router>
         )}
       </AnimatePresence>
