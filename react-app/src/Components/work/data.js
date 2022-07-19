f78d29630f756cf057a962701dd58a38a8818280
joaoguedes <joaoguedes.cjp@gmail.com>
2022-07-19 05:59:03 +0100

    Finished Work (ver 1)
    Finished Work (ver 1)

--- react-app/src/Components/work/data.js
+++ react-app/src/Components/work/data.js
@@ -9,16 +9,17 @@ export const data = [
             eng:"Web Development"
         },
         description:{
-            pt:"Este aplicativo foi construído usando a linguagem de programação R para prever com precisão o número de visitas que um shopping teria em uma determinada semana para que as campanhas de marketing apropriadas fossem configuradas durante cada dia.",
-            eng:"This app was built using the R programming language to accurately predict the number of visits a Mall would have in a certain week so the appropriate marketing campaigns would be set up during each day."
+            pt:"Este é o meu portfolio web pessoal desenhado e construido do zero, que apresenta todos os meus projetos académicos/pessoais e informações profissionais relevantes de uma forma interativa",
+            eng:"This is my personal website/ Web application where all my personal and academic projects are stored and documented, as well as all of my professional information in an interactive way"
         },
         icon: "webdev",
-        image:"./img/personal-website.png"
+        image:"./img/personal-website.png",
+        route: "/work/personalweb"
     },
     {
         title:{
             pt:"Previsões em R",
-            eng:"R Forecasting"
+            eng:"Time Series Forecasting"
         },
         subject:{
             pt:"Machine Learning",
@@ -29,7 +30,9 @@ export const data = [
             eng:"This app was built using the R programming language to accurately predict the number of visits a Mall would have in a certain week so the appropriate marketing campaigns would be set up during each day."
         },
         icon: "ml",
-        image:"./img/rForecasting.png"
+        image:"./img/rForecasting.png",
+        route: "/work/tsf"
+
     },
     {
         title:{
@@ -45,7 +48,9 @@ export const data = [
             eng:"This app was built using the R programming language to accurately predict the number of visits a Mall would have in a certain week so the appropriate marketing campaigns would be set up during each day."
         },
         icon: "ml",
-        image:"./img/esrbpredictor.png"
+        image:"./img/esrbpredictor.png",
+        route: "/work/personalweb"
+
     },
     {
         title:{
@@ -61,6 +66,8 @@ export const data = [
             eng:"This app was built using the R programming language to accurately predict the number of visits a Mall would have in a certain week so the appropriate marketing campaigns would be set up during each day."
         },
         icon: "netinf",
-        image:"./img/loadbalancer.png"
+        image:"./img/loadbalancer.png",
+        route: "/work/personalweb"
+
     }
 ]
--- react-app/src/Components/work/work.jsx
+++ react-app/src/Components/work/work.jsx
@@ -1,35 +1,43 @@
 import React from 'react'
 import './work.css'
 import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 /* Project Images */
 
 import { data } from './data'
-import {commondata} from '../common'
+import { commondata } from '../common'
 import SvgsIcons from './svgs-icons'
 
 
 const Work = (props) => {
   let lang = props.lang
+  let theme = props.theme
   return (
     <>
-      <motion.div initial={{ y: "20%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ y: "-20%", opacity: 0 }} transition={{ type: "spring", duration: 0.5 }}>
+      <motion.div initial={{ y: "20%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ y: "-20%", opacity: 0 }} transition={{ type: "spring", duration: 0.5 }} className={theme === 'light' ? "light-color" : "dark-color"}>
         <div className="title-container">
           <h1>My Work</h1>
         </div>
         <div className="work-container">
 
           {
-            data.map((item,index) => (
+            data.map((item, index) => (
               <motion.div className="work-item" key={item.title[lang]}>
                 <img className='work-item-im' src={require("" + item.image)} alt="" />
-                <div className="work-item-description">
+                <div className={theme === "light" ? "work-item-description" : "work-item-description dark-work-item-desc"}>
                   <h2>{item.title[lang]}</h2>
                   <div className='project-type'>
                     <div>{item.subject[lang]}</div>
-                    <SvgsIcons name={item.icon} />
+                    <SvgsIcons name={item.icon} theme={theme} />
                   </div>
-                  <div className='work-item-right-desc'>{item.description[lang]}</div>
-                  <a className='main-button-fill'>{commondata.expbtn[lang]}</a>
+                  <div className={theme === 'light' ? 'work-item-right-desc light-work-item' : 'work-item-right-desc dark-work-item'}>{item.description[lang]}</div>
+
+                  <div className={theme == "light" ? 'main-button-fill light-fill' : 'main-button-fill dark-fill'}>
+                    <Link className='nav-link' to={item.route}>
+                      {commondata.expbtn[lang]}
+                    </Link>
+                  </div>
+
                 </div>
               </motion.div>
             ))
