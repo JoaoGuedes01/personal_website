b23a4696cacb8fbf81326a5b55617130e78bacdf
joaoguedes <joaoguedes.cjp@gmail.com>
2022-07-19 05:58:14 +0100

    Finished Contacts (ver 1)
    Finished Contacts (ver 1)

---
+++ react-app/src/Components/contact/data.js
@@ -0,0 +1,35 @@
+export const data = {
+  pageTitle: {
+    pt: "Vamos Falar",
+    eng: "Let's Talk",
+  },
+  phone: "926909235",
+  address: {
+    pt1: "Av. Dr. Artur Soares nº23",
+    pt2: "S.Vicente, Braga",
+  },
+  email: "joaoguedes.cjp@gmail.com",
+  messageTitle: {
+    pt: "Contacte-me",
+    eng: "Send me a message",
+  },
+  form: {
+    name: {
+      pt: "nome",
+      eng: "name",
+    },
+    subject: {
+      pt: "assunto",
+      eng: "subject",
+    },
+    email: "email",
+    message: {
+      pt: "mensagem",
+      eng: "message",
+    },
+  },
+  sendbtn:{
+    pt:"Enviar",
+    eng:"Send"
+  }
+};
