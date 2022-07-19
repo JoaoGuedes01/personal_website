fd8278ebd7c337f48220b788c467dcc2f108484c
joaoguedes <joaoguedes.cjp@gmail.com>
2022-07-19 05:58:55 +0100

    Finished Navbar (ver 1)
    Finished Navbar (ver 1)

---
+++ react-app/src/Components/navbar/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        route:"/",
+        pt:"Sobre Mim",
+        eng:"About me"
+    },
+
+    {
+        route:"/work",
+        pt:"Projetos",
+        eng:"My Work"
+    },
+
+    {
+        route:"/experience",
+        pt:"Experiencia",
+        eng:"Experience"
+    },
+
+    {
+        route:"/education",
+        pt:"Educação",
+        eng:"Education"
+    },
+
+    {
+        route:"/contacts",
+        pt:"Contactos",
+        eng:"Contact me"
+    },
+
+]
--- react-app/src/Components/navbar/navbar.jsx
+++ react-app/src/Components/navbar/navbar.jsx
@@ -6,81 +6,86 @@ import { motion, AnimatePresence } from 'framer-motion'
 
 import './navbar.css'
 
-const variants = {
-  open: { x: 0 },
-  closed: { x: "-100%" }
-}
+import { data } from './data'
 
-const Navbar = () => {
+const Navbar = (props) => {
+  let lang = props.lang
+  let theme = props.theme
+
   const [isActive, setIsActive] = useState(false);
-
+  const [index, setIndex] = useState(0);
   const toggleSidebar = () => setIsActive(!isActive)
 
   return (
     <div className='navbar'>
       <div className="container">
-        <NavbarLogo className='nav-logo' />
+        <NavbarLogo className='nav-logo' theme={theme} />
         <ul className='nav-menu'>
-          <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "just", duration: 0.5, delay: 0.1 }}><Link className='nav-link' to="/">About me</Link></motion.div>
-          <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "just", duration: 0.5, delay: 0.2 }}><Link className='nav-link' to="/work">My Work</Link></motion.div>
-          <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "just", duration: 0.5, delay: 0.3 }}><Link className='nav-link' to="/experience">Experience</Link></motion.div>
-          <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "just", duration: 0.5, delay: 0.4 }}><Link className='nav-link' to="/education">Education</Link></motion.div>
-          <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "just", duration: 0.5, delay: 0.5 }}><Link className='nav-link' to="/contacts">Contact me</Link></motion.div>
-
+          {data.map((item, i) => (
+            <motion.div
+            key={item[lang]}
+              className={theme === "light" ? 'light-color' : 'dark-color'}
+              initial={{ y: "-100%", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ type: "just", duration: 0.5, delay: 0.1 * i }}
+              onClick={()=>setIndex(i)}>
+              <Link className={index == i ? 'nav-link selected' : 'nav-link'} to={{ pathname: item.route }}>
+                {item[lang]}
+              </Link>
+            </motion.div>
+          ))}
           <div className="button-container">
             <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "just", duration: 0.5, delay: 0.6 }}>
-              <a className='main-button-out'>Resume</a>
+              {
+                lang == "eng" ?
+                  <a className={theme == "light" ? 'main-button-out light-out' : 'main-button-out dark-out'}>Resume</a> :
+                  <a className={theme == "light" ? 'main-button-out light-out' : 'main-button-out dark-out'}>Currículo</a>
+              }
             </motion.div>
           </div>
         </ul>
 
         <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "just", duration: 0.5, delay: 0.1 }} className='hamb' onClick={toggleSidebar}>
           <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9.33325 16.8972H27.9999M46.6666 16.8972H37.3333M9.33325 28.3555H46.6666M9.33325 39.8139H18.6666M46.6666 39.8139H27.9999" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M9.33325 16.8972H27.9999M46.6666 16.8972H37.3333M9.33325 28.3555H46.6666M9.33325 39.8139H18.6666M46.6666 39.8139H27.9999" stroke={theme === "light" ? 'black' : 'white'} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </motion.div>
 
         <AnimatePresence exitBeforeEnter>
           {isActive && (
-            <motion.div className='hamb-menu-active' initial={{ x: "100%" }} animate={{ x: 0 }} exit={{ x: "100%" }} transition={{ type: "spring", duration: 0.5 }}>
+            <motion.div className={theme === "light" ? 'hamb-menu-active' : 'hamb-menu-active dark-menu'} initial={{ x: "100%" }} animate={{ x: 0 }} exit={{ x: "100%" }} transition={{ type: "spring", duration: 0.5 }}>
               <div className='hamb-menu-close' onClick={toggleSidebar}>
                 <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0.891113 1.07031L24.4613 24.6405M0.891113 24.6405L24.4613 1.07031" stroke="black" strokeLinecap="square" />
+                  <path d="M0.891113 1.07031L24.4613 24.6405M0.891113 24.6405L24.4613 1.07031" stroke={theme === "light" ? 'black' : 'white'} strokeLinecap="square" />
                 </svg>
               </div>
               <div className='hamb-menu-items'>
                 <ul>
-                  <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "spring", delay: 0.2 }}>
-                    <Link className='nav-link' to="/" onClick={toggleSidebar}>About me</Link>
+
+                  {
+                    data.map((item, i) => (
+                      <motion.div key={i} initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "spring", delay: 0.03 * i + 0.15 }}>
+                        <Link className='nav-link' to={{ pathname: item.route }} onClick={toggleSidebar}>{item[lang]}</Link>
+                      </motion.div>
+                    ))
+                  }
+
+                  <motion.div className="button-container" initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "spring", delay: 0.30 }}>
+                    {
+                      lang == "eng" ?
+                        <a className={theme == "light" ? 'main-button-out light-out' : 'main-button-out dark-out'}>Resume</a> :
+                        <a className={theme == "light" ? 'main-button-out light-out' : 'main-button-out dark-out'}>Currículo</a>
+                    }
                   </motion.div>
 
-                  <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "spring", delay: 0.25 }}>
-                    <Link className='nav-link' to="/work" onClick={toggleSidebar}>My Work</Link>
-                  </motion.div>
-
-                  <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "spring", delay: 0.3 }}>
-                    <Link className='nav-link' to="/experience" onClick={toggleSidebar}>Experience</Link>
-                  </motion.div>
-
-                  <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "spring", delay: 0.35 }}>
-                    <Link className='nav-link' to="/education" onClick={toggleSidebar}>Education</Link>
-                  </motion.div>
-
-                  <motion.div initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "spring", delay: 0.4 }}>
-                    <Link className='nav-link' to="/contacts" onClick={toggleSidebar}>Contact me</Link>
-                  </motion.div>
-                  
-                  <motion.div className="button-container" initial={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "spring", delay: 0.45 }}>
-                    <button className='nav-button'>Resume</button>
-                  </motion.div>
                 </ul>
               </div>
             </motion.div>
           )}
         </AnimatePresence>
 
-      </div>
-    </div>
+      </div >
+    </div >
   )
 }
 
